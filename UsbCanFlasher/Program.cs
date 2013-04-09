8825f5a115b1b4a7bf0e7fd03fb16c744d55d4fe
Evgeniy Plyusnin <eninbox@gmail.com>
2013-04-09 18:03:31 +0600

    Разбиение на порты. Вроде как, работающая версия.

--- UsbCanFlasher/Program.cs
+++ UsbCanFlasher/Program.cs
@@ -13,33 +13,41 @@ namespace UsbCanFlasher
         static void Main(string[] args)
         {
             var ds = Communications.Appi.Winusb.WinusbAppiDev.GetDevices().First();
-            var d = ds.OpenDevice();
-
-            System.Threading.Thread.Sleep(50);
-
-            d.SendMessage(CanMessage.NewWithDescriptor(0x1001, new Byte[] { 0x00 }), AppiLine.Can1);
-
-            Random r = new Random();
-
-            while (true)
+            using (var d = ds.OpenDevice())
             {
-                //d.SendMessage(CanMessage.NewWithDescriptor(0x0c08, new Byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 }), AppiLine.Can1);
-                
-                Byte[] buff = new Byte[8];
-                r.NextBytes(buff);
-                d.SendMessage(CanMessage.NewWithDescriptor(0x0c08, buff), AppiLine.Can1);
+                foreach (var p in d.Ports)
+                    p.Value.Recieved += new CanMessagesReceiveEventHandler(Value_Recieved);
 
-                System.Threading.Thread.Sleep(50);
-                foreach (var m in d.ReadMessages())
+                d.BeginListen();
+
+                Random r = new Random();
+
+                System.Threading.Thread.Sleep(1500);
+                while (Console.ReadKey().Key != ConsoleKey.Escape)
                 {
-                    if (m.Descriptor == 0x1048) Console.ForegroundColor = ConsoleColor.DarkGray;
-                    if (m.Descriptor == 0x0c08) Console.ForegroundColor = ConsoleColor.Cyan;
-                    if (m.Descriptor == 0x0c28) Console.ForegroundColor = ConsoleColor.Yellow;
-
-                    Console.WriteLine(m);
-                    Console.ResetColor();
+                    Byte[] buff = new Byte[8];
+                    r.NextBytes(buff);
+                    d.SendMessage(CanMessage.NewWithDescriptor(0x0c08, buff), AppiLine.Can1);
                 }
-                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+        static void Value_Recieved(object sender, CanMessagesReceiveEventArgs e)
+        {
+            foreach (var m in e.Messages)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(e.Line.ToString().ToUpper());
+                Console.ResetColor();
+                Console.Write(" ");
+
+                if (m.Descriptor == 0x1048) Console.ForegroundColor = ConsoleColor.DarkGray;
+                if (m.Descriptor == 0x0c08) Console.ForegroundColor = ConsoleColor.Cyan;
+                if (m.Descriptor == 0x0c28) Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine(m);
+                Console.ResetColor();
             }
         }
     }
