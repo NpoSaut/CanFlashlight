869bdfff44beec494e339fd1be8bade9157aeb6e
Evgeniy Plyusnin <eninbox@gmail.com>
2013-04-20 02:37:02 +0600

    Добавлена поддержка CanFrameHandler

--- UsbCanFlasher/Program.cs
+++ UsbCanFlasher/Program.cs
@@ -19,16 +19,47 @@ namespace UsbCanFlasher
                 foreach (var p in d.Ports.Values)
                     p.Recieved += new CanFramesReceiveEventHandler(Value_Recieved);
 
+                var h = new CanFrameHandler(0x0c28);
+                d.Ports[AppiLine.Can1].Handlers.Add(h);
+
                 d.BeginListen();
 
                 Random r = new Random();
 
                 System.Threading.Thread.Sleep(1500);
-                while (Console.ReadKey().Key != ConsoleKey.Escape)
+                
+                ConsoleKey K;
+
+                while ((K = Console.ReadKey().Key) != ConsoleKey.Escape)
                 {
-                    Byte[] buff = new Byte[8];
-                    r.NextBytes(buff);
-                    d.Ports[AppiLine.Can1].Send(CanFrame.NewWithDescriptor(0x0c08, buff));
+                    if (K == ConsoleKey.F9)
+                    {
+                        d.Ports[AppiLine.Can1].Send(CanFrame.NewWithDescriptor(0x1001, new Byte[] { 0x01 }));
+                    }
+                    else if (K == ConsoleKey.F10)
+                    {
+                        d.Ports[AppiLine.Can1].Send(CanFrame.NewWithDescriptor(0x1001, new Byte[] { 0x00 }));
+                    }
+                    else if (K == ConsoleKey.F11)
+                    {
+                        for (int j = 0; j < 10; j++)
+                            d.Ports[AppiLine.Can1].Send(
+                                Enumerable.Range(0, 100)
+                                    .Select(i => CanFrame.NewWithDescriptor(0x2008, BitConverter.GetBytes((long)(i*j))))
+                                    .ToList());
+                        Console.WriteLine("sent");
+                    }
+                    else
+                    {
+                        Byte[] buff = new Byte[8];
+                        r.NextBytes(buff);
+                        d.Ports[AppiLine.Can1].Send(CanFrame.NewWithDescriptor(0x0c08, buff));
+                        var dt1 = DateTime.Now;
+                        var rxb = h.WaitFor();
+                        var dt2 = DateTime.Now;
+                        Console.Write("{0}ms ", (dt2 - dt1).TotalMilliseconds);
+                        Console.WriteLine(buff.SequenceEqual(rxb.Data) ? "ok" : "error");
+                    }
                 }
             }
         }
