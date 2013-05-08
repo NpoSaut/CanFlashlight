4304538d2d0a8a042522b9ddcb3c908c20752929
Evgeniy Plyusnin <eninbox@gmail.com>
2013-05-08 19:22:54 +0600

    Кажется, теперь работает ISO-TP Конечно, надо дебажить ещё, но что-то уже есть!

--- UsbCanFlasher/Program.cs
+++ UsbCanFlasher/Program.cs
@@ -6,6 +6,7 @@ using MadWizard.WinUSBNet;
 using System.IO;
 using Communications.Appi;
 using Communications.Can;
+using Communications.Protocols.IsoTP;
 
 namespace UsbCanFlasher
 {
@@ -16,14 +17,36 @@ namespace UsbCanFlasher
             var ds = Communications.Appi.Winusb.WinusbAppiDev.GetDevices().First();
             using (var d = ds.OpenDevice(true))
             {
-                using (var b = new CanFramesBuffer(0x1048, d.Ports.Values))
-                {
-                    foreach (var f in b.Read(TimeSpan.FromMilliseconds(1500)))
-                        Console.WriteLine(f);
-                }
+                d.Ports[AppiLine.Can1].Recieved += new CanFramesReceiveEventHandler(Program_Recieved);
+
+                TpRecieveTransaction rt = new TpRecieveTransaction(d.Ports[AppiLine.Can1], 0x3008) { SeparationTime = TimeSpan.FromMilliseconds(50) };
+
+                var tt = System.Threading.Tasks.Task<byte[]>.Factory.StartNew(rt.Recieve);
+
+                Byte[] data = new Byte[4095];
+                (new Random()).NextBytes(data);
+
+                Console.WriteLine("Подготавливаемся");
+                TpPacket sp = new TpPacket(data);
+                TpSendTransaction st = new TpSendTransaction(d.Ports[AppiLine.Can1], 0x3008);
+                Console.WriteLine("Начинаем отправку");
+                st.Send(sp);
+
+                tt.Wait();
+                var xxx = tt.Result;
+                if (xxx.SequenceEqual(data)) Console.WriteLine("Yeeeaaaaahhhhhh!! =)");
+                else Console.WriteLine("=(");
+
                 Console.WriteLine("----------------------");
+
                 Console.Read();
             }
+        }
+
+        static void Program_Recieved(object sender, CanFramesReceiveEventArgs e)
+        {
+            foreach (var f in e.Frames.Where(f => f.Descriptor == 0x3008))
+                Console.WriteLine("{1} {0}", f, f.IsLoopback ? "*" : " ");
         }
     }
 }
